fd305d9129ce06354f628a601906c027e29230dc
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-01-03 20:48:18 +0000

    Style the skill, service, project and contact sections

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+const openModals = document.querySelectorAll('.open-modal');
+const closeModals = document.querySelectorAll('.close-modal');
+
+openModals.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentNode.parentNode.querySelector('.modal').showModal();
+    });
+});
+
+closeModals.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentNode.close();
+    });
+});
