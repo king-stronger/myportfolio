e0cdba6aa1c46f1f04edc037c9addce680dd4c21
Stronger Alaefule <114509657+king-stronger@users.noreply.github.com>
2025-01-07 15:49:11 +0000

    Add a navigation section for mobile with hthe respectives css styles, modify the projects section with relevant data and images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+
+/**
+ * Add an event listener to the menu button to toggle the menu on mobile
+ */
+const menuButton = document.querySelector('.button-menu');
+const menu = document.getElementById('nav');
+
+menuButton.addEventListener('click', () => {
+    let navMobile = menu.classList.contains('nav-mobile');
+
+    if(navMobile){
+        menu.classList.add('nav');
+        menu.classList.remove('nav-mobile');
+    } else {
+        menu.classList.remove('nav');
+        menu.classList.add('nav-mobile');
+    }
+});
+
+/**
+ * Add an event listener to the language button to to toggle the language menu
+ */
 const languageButton = document.querySelector('.language-button');
 const languageMenu = document.querySelector('#language-menu');
 
@@ -9,7 +31,9 @@ languageButton.addEventListener('click', () => {
 });
 
 
-
+/**
+ * Add an event listener to modals button to toggle the respective modal
+ */
 const openModals = document.querySelectorAll('.open-modal');
 const closeModals = document.querySelectorAll('.close-modal');
 
